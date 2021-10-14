f5fddab6a9c601883cb3cd5bb1071e188dfe91c2
Maks <Delaerous12@yandex.ru>
2021-10-14 10:43:30 +0300

    fix img

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,15 +1,9 @@
-// let swiper2 = new Swiper(".carousel__list", {
-//     slidesPerView: 5,
-//     spaceBetween: 30,
-//     slidesPerGroup: 5,
-//     loop: true,
-//     loopFillGroupWithBlank: true,
-//   });
+
 
 const slider1 = document.querySelector('.swiper-containerjs');
 
   let mySwiper1 = new Swiper(slider1, {
-	slidesPerView: 3,
+	slidesPerView: 5,
         spaceBetween: 30,
 	loop: true,
     
